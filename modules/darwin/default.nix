1fced3533f5f368f47974d696ec2acf5184cbc1d
Sped0n <hi@sped0n.com>
2025-04-13 18:17:20 +0800

    fix: re-enable Mac App Store apps for macOS 15.4
    - Re-enable multiple Mac App Store applications previously commented out due to macOS 15.4 compatibility issues
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -37,21 +37,20 @@
     # you may receive an error message "Redownload Unavailable with This Apple ID".
     # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
     masApps = {
-      # FIXME: mas is broken on macOS 15.4
-      #"Bitwarden" = 1352778147;
-      #"WeChat" = 836500024;
-      #"WhatsApp Messenger" = 310633997;
-      #"Endel" = 1346247457;
-      #"Windows App" = 1295203466;
-      #"Dropover" = 1355679052;
-      #"Microsoft Word" = 462054704;
-      #"Microsoft Excel" = 462058435;
-      #"Microsoft PowerPoint" = 462062816;
-      #"Microsoft Outlook" = 985367838;
-      #"Microsoft OneDrive" = 823766827;
-      #"PDFgear" = 6469021132;
-      #"Photomator" = 1444636541;
-      #"VidHub" = 1659622164;
+      "Bitwarden" = 1352778147;
+      "WeChat" = 836500024;
+      "WhatsApp Messenger" = 310633997;
+      "Endel" = 1346247457;
+      "Windows App" = 1295203466;
+      "Dropover" = 1355679052;
+      "Microsoft Word" = 462054704;
+      "Microsoft Excel" = 462058435;
+      "Microsoft PowerPoint" = 462062816;
+      "Microsoft Outlook" = 985367838;
+      "Microsoft OneDrive" = 823766827;
+      "PDFgear" = 6469021132;
+      "Photomator" = 1444636541;
+      "VidHub" = 1659622164;
     };
   };
 
